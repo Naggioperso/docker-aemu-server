5f376c94a25d1bc13ad0a0e8108458bd788a3766
Valerio <212555650+Naggioperso@users.noreply.github.com>
2026-02-14 00:46:29 +0100

    added base for server
    addedthe necessary to start a server

---
+++ get-aemu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+OWNER="Kethen"
+REPO="aemu"
+API_URL="https://api.github.com/repos/$OWNER/$REPO/releases/latest"
+RELEASE_DATA=$(curl -s $API_URL)
+TAG_NAME=$(echo "$RELEASE_DATA" | jq -r '.tag_name')
+DOWNLOAD_URL=$(echo "$RELEASE_DATA" | jq -r '.assets[] | select(.name | startswith("aemu_")) | .browser_download_url')
+
+
+if [ -z $DOWNLOAD_URL ]
+then
+    echo "No download URL could be found."
+    exit 1
+fi
+
+FILENAME=$(basename "$DOWNLOAD_URL")
+
+echo "Downloading $FILENAME"
+wget -O "$FILENAME" "$DOWNLOAD_URL"
+unzip -d aemu "$FILENAME"
+rm -v "$FILENAME"
