7f200e1e4d09d4eeb49df1a778f25ba3cfa92d4c
Naggioperso <212555650+Naggioperso@users.noreply.github.com>
2026-02-16 21:24:44 +0100

    more changes to init.sh

--- init.sh
+++ init.sh
@@ -16,13 +16,14 @@ mkdir -pv ./logs
 report_ok(){
     echo -e "
 Postoffice download status: $DL_1
-Aemu download status: $DL_2\n\n
+Aemu download status: $DL_2
+
 ${YELLOW}!!! WARNING!!!${RESET_CLR}
-The webserver exposing the status page is running in it's own container to make it independent from the main components. The default webserver uses the localhost address (127.0.0.1) to fetch data for the status page, therefore, it will not work unless you edit the server.js, substituting the localhost with the IP of the container running the postoffice software. You can choose to:
+The webserver is running in it's own container to make it independent from the main components. The default webserver uses the localhost address (127.0.0.1) to fetch data for the status page, therefore, it will not work unless you edit the server.js, substituting the localhost with the IP of the container running the postoffice software. You can choose to:
 
 \t1. Stop now, customize the configurations to your likings and build the containers later\n
 \t2. Continue with the default configurations and apply the customizations later.\n"
-
+   
     sleep 3
 }
 
@@ -32,7 +33,7 @@ Postofffice download status: $DL_1
 $GET_POSTOFFICE_MSG\n
 Aemu download status: $DL_2
 $GET_AEMU_MSG\n
-Some errors were encountered. The process is going to stop as the requirements to run the containers might not be satisfied. Fxi the issues and then run 'docker compose up -d --build' to start the process.\n\n
+Some errors were encountered. The process is going to stop as the requirements to run the containers might not be satisfied. Check the logs fix the issues and then run 'docker compose up -d --build' to start the process.\n\n
 ${YELLOW}!!! WARNING!!!${RESET_CLR}
 The webserver exposing the status page will run in it's own container to make it independent from the main components. The default webserver uses the localhost address (127.0.0.1) to fetch data for the status page, therefore, it will not work unless you edit the server.js, substituting the localhost with the IP of the container running the postoffice software."
     sleep 2
@@ -65,9 +66,9 @@ GET_POSTOFFICE_EXIT_CODE=$?
 if [ $GET_POSTOFFICE_EXIT_CODE != 0 ]
 then
     GET_POSTOFFICE_MSG="An error occurred during tha download of postoffice. Try to run the scritp 'get-postoffice.sh' again or download it manually.\nFor more details, check the logs './logs/get-postoffice.log'."
-    DL_1=$DL_ERROR
+    DL_1=${RED}$DL_ERROR${RESET_CLR}
 else
-    DL_1=$DL_OK
+    DL_1=${GREEN}$DL_OK${RESET_CLR}
 fi
 
 ./get-aemu.sh > ./logs/get-aemu.log 2>&1
@@ -75,9 +76,9 @@ GET_AEMU_EXIT_CODE=$?
 if [ $GET_AEMU_EXIT_CODE != 0 ]
 then
     GET_AEMU_MSG="An error occurred during tha download of aemu. Try to run the scritp 'get-aemu.sh' again or download it manually.\nFor more details, check the logs './logs/get-aemu.log'."
-    DL_2=$DL_ERROR
+    DL_2=${RED}$DL_ERROR${RESET_CLR}
 else
-    DL_2=$DL_OK
+    DL_2=${GREEN}$DL_OK${RESET_CLR}
 fi
 
 
