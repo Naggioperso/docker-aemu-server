cb0a56bb31e729a28d56009cc0faf461583b99ca
Naggioperso <212555650+Naggioperso@users.noreply.github.com>
2026-02-16 23:44:18 +0100

    more changes to the ini.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-#Some clours for the output
+#Some colors for the output
 RED="\033[0;31m"
 GREEN="\033[0;32m"
 YELLOW="\033[0;33m"
@@ -10,21 +10,16 @@ DL_ERROR="ERROR"
 DL_OK="OK"
 
 
-mkdir -pv ./logs
-
-
 report_ok(){
     echo -e "
 Postoffice download status: $DL_1
-Aemu download status: $DL_2
-
+Aemu download status: $DL_2\n
 ${YELLOW}!!! WARNING!!!${RESET_CLR}
-The webserver is running in it's own container to make it independent from the main components. The default webserver uses the localhost address (127.0.0.1) to fetch data for the status page, therefore, it will not work unless you edit the server.js, substituting the localhost with the IP of the container running the postoffice software. You can choose to:
-
+The webserver is running in it's own container to make it independent from the main components. The default webserver uses the localhost address (127.0.0.1) to fetch data for the status page, therefore, it will not work unless you edit the server.js, substituting the localhost with the IP of the container running the postoffice software. You can choose to:\n
 \t1. Stop now, customize the configurations to your likings and build the containers later\n
 \t2. Continue with the default configurations and apply the customizations later.\n"
-   
-    sleep 3
+
+    sleep 5
 }
 
 report_error(){
@@ -36,12 +31,14 @@ $GET_AEMU_MSG\n
 Some errors were encountered. The process is going to stop as the requirements to run the containers might not be satisfied. Check the logs fix the issues and then run 'docker compose up -d --build' to start the process.\n\n
 ${YELLOW}!!! WARNING!!!${RESET_CLR}
 The webserver exposing the status page will run in it's own container to make it independent from the main components. The default webserver uses the localhost address (127.0.0.1) to fetch data for the status page, therefore, it will not work unless you edit the server.js, substituting the localhost with the IP of the container running the postoffice software."
+    
     sleep 2
 }
 
 
 bring_up(){
     #Building the images
+    echo "Starting to build the custom images\n"
     if ! docker compose build
     then
         echo -e "An error occurred during the build of the images. Check the logs in ./logs/bring-up.logs"
@@ -49,6 +46,7 @@ bring_up(){
     fi
 
     #Staring the containers
+    echo "Starting the containers\n"
     if ! docker compose up -d
     then
         echo -e "An error occurred during the start of the containers. Check the logs in ./logs/bring-up.logs"
@@ -56,12 +54,24 @@ bring_up(){
     fi
 
     # Bring-up finished
-    echo -e "Changing the IP in the server.js file will only require the restart of the the webserver with 'docker compose restart websrv'"
+    echo -e "${GREEN}Comlpeted!${RESET_CLR}"
 }
 
 
+check_logdir(){
+    if [ -d ./logs ]
+    then
+        echo "Removing old logs if present"
+        rm -v ./logs/*
+    else
+        mkdir -v ./logs
+    fi
+}
 
-./get-postoffice.sh > ./logs/get-postoffice.log 2>&1
+
+check_logdir 2>&1 | tee -a ./logs/init.log
+
+./get-postoffice.sh 2>&1 | tee ./logs/get-postoffice.log
 GET_POSTOFFICE_EXIT_CODE=$?
 if [ $GET_POSTOFFICE_EXIT_CODE != 0 ]
 then
@@ -71,7 +81,7 @@ else
     DL_1=${GREEN}$DL_OK${RESET_CLR}
 fi
 
-./get-aemu.sh > ./logs/get-aemu.log 2>&1
+./get-aemu.sh 2>&1 | tee ./logs/get-aemu.log
 GET_AEMU_EXIT_CODE=$?
 if [ $GET_AEMU_EXIT_CODE != 0 ]
 then
@@ -84,24 +94,25 @@ fi
 
 if [ "$DL_1" = "OK" ] && [ "$DL_2" = "OK" ]
 then
-    report_ok > ./logs/init.log 2>&1
+    report_ok 2>&1 | tee ./logs/init.log
     read -p "Chose how to proceed (1,2):" option
     case $option in
         1)
-            echo -e "\nWhen you're ready, run the following command:\n\t'docker compose up -d --build'"
+            echo "User chose option 1\nWhen you're ready, run the following command:\n\t'docker compose up -d --build'" | tee -a ./logs/init.log
             sleep 1
             exit 0
             ;;
         2)
-            echo -e "\nStaring the build process"
-            bring_up > ./logs/bring-up.logs 2>&1
+            echo -e "User chose option 2\nStaring the build process..." | tee -a ./logs/init.log
+            bring_up 2>&1 | tee -a ./logs/bring-up.logs
+            echo -e "The bring-up is finished\n\nChanging the IP in the server.js file will only require the restart of the the webserver with 'docker compose restart websrv'. Before making further changes, I suggest you to make sure you understand the implications."
             exit 0
             ;;
         *)
-            echo "Choose between option 1 or 2."
+            echo "Choose between option 1 and 2."
             ;;
     esac
 else
-    report_error > ./logs/init.log 2>&1
+    report_error 2>&1 | tee -a ./logs/init.log
     exit 1
 fi
